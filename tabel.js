404e2dd82bd3f7f1cc3400020ae0f235b3afa2c8
Syarif09 <syarif.khalid@gmail.com>
2024-07-06 19:26:25 +0700

    input table & CSS

--- tabel.js
+++ tabel.js
@@ -1,26 +1,78 @@
 const myButton = document.getElementById('button')
 myButton.addEventListener('click', processData)
 
-function processData(event){
+const myForm = document.getElementById('data-survey')
+
+function processData(event) {
     event.preventDefault()
     const tBody = document.getElementById('inputData')
     const addData = document.createElement('tr')
     const dataName = document.createElement('td')
     const dataAge = document.createElement('td')
     const dataGender = document.createElement('td')
-    const dataIsSmoke = document.createElement('td')
-    const dataVariant = document.createElement('td')
-    const name = document.getElementById('name').value
-    const age = document.getElementById('age').value
-    const gender = document.getElementsByName('gender').value
-    const isSmoke = document.getElementsByName('smoke').value
-    const variant = document.getElementsByName('variant').value
-    console.log()
-    // dataName.appendChild(name)
-    // dataAge.appendChild(age)
-    // dataGender.appendChild(gender)
-    // dataIsSmoke.appendChild(isSmoke)
-    // dataVariant.appendChild(variant)
-    // addData.appendChild(dataName)
-    // tBody.appendChild(addData)
+    const dataSmoke = document.createElement('td')
+    const dataBrand = document.createElement('td')
+
+    // name
+    const name = document.getElementById('name')
+    const valueName = name.value
+    dataName.textContent = valueName
+
+    // Age
+    const age = document.getElementById('age')
+    const valueAge = age.value
+    dataAge.textContent = valueAge
+
+    // Gender
+    const male = document.getElementById('male').checked
+    const female = document.getElementById('female').checked
+    if (male == true) {
+        const maleData = document.getElementById('male')
+        const maleValue = maleData.value
+        dataGender.textContent = maleValue
+    }else if (female == true) {
+        const femaleData = document.getElementById('female')
+        const femaleValue = femaleData.value
+        dataGender.textContent = femaleValue
+    }
+
+    // isSmoke
+    const smoke = document.getElementById('yes').checked
+    const noSmoke = document.getElementById('no').checked
+    if (smoke == true) {
+        const smokeData = document.getElementById('yes')
+        const smokeValue = smokeData.value
+        dataSmoke.textContent = smokeValue
+    }else if (noSmoke == true) {
+        const noSmokeData = document.getElementById('no')
+        const noSmokeValue = noSmokeData.value
+        dataSmoke.textContent = noSmokeValue
+        console.log(noSmokeValue)
+    }
+
+    // Cigar Variant
+
+    const brand = document.getElementsByName('variant')
+    let result = []
+    for (let i = 0; i < brand.length; i++){
+        if (brand[i].checked == true) {
+            result.push(brand[i].value)
+        }
+    }
+
+    let brandValue = ''
+    for (let i = 0; i < result.length; i++){
+        brandValue += result[i]+'; '
+    }
+    console.log(brandValue)
+    dataBrand.textContent = brandValue
+
+
+    addData.appendChild(dataName)
+    addData.appendChild(dataAge)
+    addData.appendChild(dataGender)
+    addData.appendChild(dataSmoke)
+    addData.appendChild(dataBrand)
+    tBody.appendChild(addData)
+    myForm.reset()
 }
